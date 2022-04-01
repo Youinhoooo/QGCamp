357f069d658e6a9af27e3cdcd6c63fe998fd0d2e
Youinhoooo <1372383120@qq.com>
2022-04-01 22:12:40 +0800

    week2

--- small_group/Week_2/linearRegression.py
+++ small_group/Week_2/linearRegression.py
@@ -3,37 +3,46 @@ import numpy as np
 
 class linearRegression:
     # 最小二乘法
-    def fit(self, X, y):
-        X = np.asmatrix(X.copy())  # X转换为矩阵
-        y = np.asmatrix(y).reshape(-1, 1)  # y转换为向量
-        # 通过公式推导得出
-        self.w = (X.T * X).I * X.T * y
+    def __init__(self,X,y):
+        self.X = X
+        self.y = y
 
-    def predict(self, X):
+    def fit(self):
+        _X = np.asmatrix(self.X.copy())  # X转换为矩阵
+        _y = np.asmatrix(self.y).reshape(-1, 1)
+        # 通过公式推导得出
+        self.w = (_X.T * _X).I * _X.T * _y
+
+    def predict(self,X):
         X = np.asmatrix(X.copy())
         result = X * self.w
         return np.asarray(result).ravel()  # 进行扁平化处理
-        # 梯度下降法
 
-    def cost_function(self, data, theta, y):
-        cost = np.sum((data.dot(theta) - y) ** 2)
-        #m = data.shape[1]
+        # 梯度下降法
+    def cost_function(self, theta):
+        X = np.array(self.X)
+        y = np.array(self.y)
+        cost = np.sum((X.dot(theta) - y) ** 2)
+        #cost = 1
+        m = len(theta)
         return cost / (2 * m)
 
-    def gradient(self, data, theta, y):
+    def gradient(self, theta):
+        X = np.array(self.X)
+        y = np.array(self.y)
         grad = np.empty(len(theta))
-        grad[0] = np.sum(data.dot(theta) - y)
+
+        grad[0] = np.sum(X.dot(theta) - y)
         for i in range(1, len(theta)):
-            grad[i] = (data.dot(theta) - y).dot(data[:, i])
+            temp = np.dot(X,theta)-y
+            grad[i] = np.dot(temp,X[:,i])
         return grad
 
-    def gradient_descent(self, data, theta, y, eta):
-        while True:
+    def gradient_descent(self, theta, eta, times):
+        for i in range(times):
             last_theta = theta
-            #y = np.asarray(y)
-            grad = linearRegression.gradient(self,data, theta, y)
+            grad = self.gradient(theta)
             theta = theta - eta * grad
-            print(theta)
-            if abs(linearRegression.cost_function(self,data, last_theta, y) - linearRegression.cost_function(self,data, theta, y)) < 1e-15:
+            if abs(self.cost_function(last_theta) - self.cost_function(theta)) < 1e-15:
                 break
         return theta
